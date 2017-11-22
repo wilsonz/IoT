e99c6668819094fae3fcce6bef13430b4abb6e70
wilsonz <wilsonxyz@qq.com>
2017-11-22 15:36:23 -0500

    Create 8. models.py

---
+++ Django/8. models.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+class TemperatureData(models.Model):
+    date_time = models.CharField(max_length=30)
+    temperature = models.CharField(max_length=5)
+    latitude = models.CharField(max_length=20)
+    longitude = models.CharField(max_length=20)
+    def __unicode__(self):                        --> __str__ in python3
+        return self.date_time
